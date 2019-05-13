4115e5c30c8e506ca63690bd5f6bd04b44617a81
Chris Turner <chris@Chriss-MacBook-Pro.local>
2019-05-13 14:00:51 -0400

    Fourth Version

--- index.php
+++ index.php
@@ -7,11 +7,12 @@
 
 <body>
 	
-	This is the fourth version of my git project - let's pretend this was authored by Chris.
+	This is the fourth version of my git project.
 	
-	<p>
-		Chris is the author.
-	</p>
+	
+	<div>
+		Now some crazy guy named Anthony came along and worked in the same file as Chris.  What happens next when I try to merge all this junk together?
+	</div>
 	
 </body>
 </html>
